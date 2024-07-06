e0c8744d8e12a4f472aceacb0d4872dceece959f
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2024-07-06 17:38:49 -0600

    Modificaicon de clase principal para usar las nuevas clases

--- src/main/java/com/practica/api_libros/principal/Principal.java
+++ src/main/java/com/practica/api_libros/principal/Principal.java
@@ -1,13 +1,26 @@
 package com.practica.api_libros.principal;
 
+import com.practica.api_libros.model.ApiResponse;
+import com.practica.api_libros.model.DatosLibro;
 import com.practica.api_libros.service.ConsumoAPI;
+import com.practica.api_libros.service.ConvierteDatos;
+
+import java.util.List;
 
 public class Principal {
     private ConsumoAPI consumoAPI = new ConsumoAPI();
-    private final String URL = "https://gutendex.com/books";
+//    private final String URL = "https://gutendex.com/books";
+    private final String URL ="https://gutendex.com/books/?search=Hyde%20Stevenson";
+    private ConvierteDatos conversor = new ConvierteDatos();
 
     public void prueba(){
         var json = consumoAPI.obtenerDatos(URL);
-        System.out.println(json);
+
+
+        var apiResponse = conversor.obtenerDatos(json, ApiResponse.class);
+        DatosLibro primerLibro = apiResponse.results().get(0);
+
+        System.out.println(primerLibro);
+
     }
 }
