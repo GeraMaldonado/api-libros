b2ff851b35a183fbc27eb5a87971ab2b8d352465
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2024-07-05 23:12:42 -0600

    Adecuacion para funcionar en consola y llamar a una clase principal

--- src/main/java/com/practica/api_libros/ApiLibrosApplication.java
+++ src/main/java/com/practica/api_libros/ApiLibrosApplication.java
@@ -1,13 +1,20 @@
 package com.practica.api_libros;
 
+import com.practica.api_libros.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiLibrosApplication {
+public class ApiLibrosApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiLibrosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.prueba();
+	}
 }
