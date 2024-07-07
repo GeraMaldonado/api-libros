1ba78462cebb124667a21830ca744bea6450a071
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2024-07-07 15:11:20 -0600

    Metodos para usar las clases libro y persona y la base de datos

--- src/main/java/com/practica/api_libros/principal/Principal.java
+++ src/main/java/com/practica/api_libros/principal/Principal.java
@@ -1,24 +1,34 @@
 package com.practica.api_libros.principal;
 
 import com.practica.api_libros.model.ApiResponse;
+import com.practica.api_libros.model.Autor;
+import com.practica.api_libros.model.DatosAutor;
 import com.practica.api_libros.model.DatosLibro;
+import com.practica.api_libros.model.Libro;
+import com.practica.api_libros.repository.LibroRepository;
 import com.practica.api_libros.service.ConsumoAPI;
 import com.practica.api_libros.service.ConvierteDatos;
-
-import java.util.List;
+import org.springframework.stereotype.Component;
+import java.util.Optional;
 import java.util.Scanner;
 
+@Component
 public class Principal {
     private Scanner escaner = new Scanner(System.in);
     private ConsumoAPI consumoAPI = new ConsumoAPI();
     private final String URL = "https://gutendex.com/books/?search=";
     private ConvierteDatos conversor = new ConvierteDatos();
+    private LibroRepository repositorio;
 
-    public void muestraMenu(){
+    public Principal(LibroRepository repository) {
+        this.repositorio = repository;
+    }
+
+    public void muestraMenu() {
         var opcion = -1;
-        while (opcion != 0){
+        while (opcion != 0) {
             var menu = """
-                    1 - Buscar libro por titulo
+                    1 - Buscar libro por título
                     2 - Lista libros registrados
                     3 - Lista autores registrados
                     4 - Lista autores vivos en un determinado año
@@ -30,7 +40,7 @@ public class Principal {
             opcion = escaner.nextInt();
             escaner.nextLine();
 
-            switch (opcion){
+            switch (opcion) {
                 case 1:
                     buscarLibroWeb();
                     break;
@@ -39,31 +49,45 @@ public class Principal {
                     System.out.println("Cerrando la aplicación...");
                     break;
                 default:
-                    System.out.println("Opcion invalida");
+                    System.out.println("Opción inválida");
             }
         }
     }
 
-
-
-
-
-
-
-
-
-    private DatosLibro getDatosLibro(){
-        System.out.println("Escribe el titulo del libro que deseas buscar");
+    private DatosLibro getDatosLibro() {
+        System.out.println("Escribe el título del libro que deseas buscar");
         var nombreLibro = escaner.nextLine();
-        var json = consumoAPI.obtenerDatos(URL + nombreLibro.replace(" ","%20"));
+        var json = consumoAPI.obtenerDatos(URL + nombreLibro.replace(" ", "%20"));
         var apiResponse = conversor.obtenerDatos(json, ApiResponse.class);
+        if(apiResponse.results().isEmpty()){
+            System.out.println("Libro no encontrado");
+            muestraMenu();
+        };
         DatosLibro libro = apiResponse.results().get(0);
         return libro;
     }
 
     private void buscarLibroWeb() {
         DatosLibro datos = getDatosLibro();
-        System.out.println("Desde el metodo");
-        System.out.println(datos);
+        if (datos != null) {
+            DatosAutor datosAutor = datos.autorList().get(0);
+
+            Optional<Autor> autorExistente = repositorio.findAutorByNombre(datosAutor.nombre());
+            Autor autor;
+
+            if (autorExistente.isPresent()) {
+                autor = autorExistente.get();
+            } else {
+                repositorio.saveAutor(datosAutor.nombre(), datosAutor.nacimiento(), datosAutor.fallecimiento());
+                autor = repositorio.findAutorByNombre(datosAutor.nombre()).get();
+            }
+
+            Libro libro = new Libro(datos);
+            libro.setAutor(autor);
+            repositorio.save(libro);
+            System.out.println(libro);
+        } else {
+            System.out.println("No se encontró ningún libro.");
+        }
     }
 }
