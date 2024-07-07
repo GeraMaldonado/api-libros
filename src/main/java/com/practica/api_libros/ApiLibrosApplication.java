5f7326ede3913b2ab00d3c8d10159584c5f55417
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2024-07-07 17:22:55 -0600

    Inyeccion de dependencias

--- src/main/java/com/practica/api_libros/ApiLibrosApplication.java
+++ src/main/java/com/practica/api_libros/ApiLibrosApplication.java
@@ -1,6 +1,7 @@
 package com.practica.api_libros;
 
 import com.practica.api_libros.principal.Principal;
+import com.practica.api_libros.repository.AutorRepository;
 import com.practica.api_libros.repository.LibroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,14 +11,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiLibrosApplication implements CommandLineRunner {
 	@Autowired
-	private LibroRepository repository;
+	private LibroRepository libroRepository;
+	@Autowired
+	private AutorRepository autorRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ApiLibrosApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repository);
+		Principal principal = new Principal(libroRepository, autorRepository);
 		principal.muestraMenu();
 	}
 }
