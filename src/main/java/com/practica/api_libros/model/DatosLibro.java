cc4a3c92a5a8879f444418be5ac5fdcf0502c556
Gerardo Maldonado Félix <gmaldonadofelix@gmail.com>
2024-07-07 15:10:46 -0600

    Limpieza de codigo

--- src/main/java/com/practica/api_libros/model/DatosLibro.java
+++ src/main/java/com/practica/api_libros/model/DatosLibro.java
@@ -2,12 +2,11 @@ package com.practica.api_libros.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosLibro(
-        @JsonAlias("title") String title,
+        @JsonAlias("title") String titulo,
         @JsonAlias("authors") List<DatosAutor> autorList,
         @JsonAlias("languages") List<String> idioma,
         @JsonAlias("download_count") Long descargas
